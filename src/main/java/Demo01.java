bf18771eea5b246bb67f3bbe7f3caa85b9c5e0dc
honey <69959803+honey718@users.noreply.github.com>
2020-08-20 17:13:20 +0800

    Update Demo01.java

--- src/main/java/Demo01.java
+++ src/main/java/Demo01.java
@@ -1,6 +1,6 @@
 public class Demo01 {
     public static void main(String[] args) {
-        int [] arr = {1,2,55,58,6,5,5};
+        int [] arr = {1,2,55,58,6,5,5,89};
         for (int i : arr
              ) {
             System.out.println(i);
