92c8a2ba1acbb755d12f81482d107036c9a9f0f8
86153 <386932873@qq.com>
2020-08-20 17:11:17 +0800

    Initial commit

---
+++ src/main/java/Demo01.java
@@ -0,0 +1,9 @@
+public class Demo01 {
+    public static void main(String[] args) {
+        int [] arr = {1,2,55,58,6,5,5};
+        for (int i : arr
+             ) {
+            System.out.println(i);
+        }
+    }
+}
